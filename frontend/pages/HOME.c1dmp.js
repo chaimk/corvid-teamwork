996862117d09b4f2c4a3ec8c4ba61b6d392c14ca
Chaim Kram <chaimk@wix.com>
2019-04-08 12:20:42 +0300

    Initial commit

---
+++ frontend/pages/HOME.c1dmp.js
@@ -0,0 +1,10 @@
+// For full API documentation, including code examples, visit http://wix.to/94BuAAs
+
+$w.onReady(function () {
+	//TODO: write your page related code here...
+
+});
+
+export function image1_click(event) {
+	console.log("Uriya is the bomb")
+}
